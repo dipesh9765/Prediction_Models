e995d43be27747d87687882a104ed8fa43c14e33
dipesh9765 <dipeshmarathe9765@gmail.com>
2023-07-02 12:56:11 +0530

    all the backend is done only frontend of some models remaining

--- website/views.py
+++ website/views.py
@@ -27,6 +27,9 @@ car_scalar = pickle.load(open('./website/Car/c_scaling.pkl', 'rb'))
 medical_regmodel = pickle.load(open('./website/Medical_Insurance/m_regmodel.pkl','rb'))
 medical_scalar = pickle.load(open('./website/Medical_Insurance/m_scaling.pkl', 'rb'))
 
+calorie_regmodel = pickle.load(open('./website/Exercise/e_regmodel.pkl','rb'))
+calorie_scalar = pickle.load(open('./website/Exercise/e_scaling.pkl', 'rb'))
+
 
 
 @views.route('/House') 
@@ -44,6 +47,10 @@ def diabetes():
 @views.route('/Medical-Insurance')
 def medical_insurance():
     return render_template('medical.html')
+
+@views.route('/Calorie-Predictor')
+def calorie_predictor():
+    return render_template('exercise.html')
 
 
 #HOUSE PREDICT API
@@ -141,4 +148,28 @@ def m_predict():
     print(output) 
     output = format(output,".2f")
     return render_template("medical.html", prediction_text="The Medical Insurance predicted  price is ₹ {}".format(output))
+
+
+#CALORIE PREDICT API
+@views.route('/Calorie-Predictor/e_predict_api', methods=['POST'])
+def e_predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = calorie_scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = calorie_regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+#CALORIE PREDICT POST METHOD
+@views.route('/e_predict', methods=['POST'])
+def e_predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = calorie_scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = calorie_regmodel.predict(final_input)[0] 
+    print(output) 
+    output = format(output,".2f")
+    return render_template("exercise.html", prediction_text="Calorie Burnt :  {} Kcal".format(output))
 
